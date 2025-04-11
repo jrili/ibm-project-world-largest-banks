a81570959f04b437da7a70234c9b93c2e93c8696
jrili <jfmrili@gmail.com>
2025-04-11 20:34:51 +0800

    Transfer DB utility function/s from banks_project.py to db_utils.py

--- banks_project.py
+++ banks_project.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import sqlite3
 
 import config
@@ -6,36 +5,33 @@ from utils.logging_utils import logging_utils
 from utils.etl_utils import extract_utils
 from utils.etl_utils import transform_utils
 from utils.etl_utils import load_utils
+from utils.db_utils import db_utils
 
-############ DB Methods ############
-def run_query(query_statement, sql_connection):
-    logging_utils.log_progress(f"run_query(): started, query='{query_statement}'")
-    query_output = pd.read_sql(query_statement, sql_connection)
-
-    logging_utils.log_progress(f"run_query(): query: {query_statement}")
-    logging_utils.log_progress(f"run_query(): output:\n{query_output.to_string()}")
-    logging_utils.log_progress(f"Process Complete")
-
-
-############ Main Process ############
+# Set log file path
 logging_utils.set_path_to_logfile(config.PATH_TO_LOGFILE)
 
-logging_utils.log_progress("Preliminaries complete. Initiating ETL process")
-
+# Extract data
+logging_utils.log_progress("ETL prcess started. Extracting data...")
 extracted_df = extract_utils.extract(url=config.WEBSCRAPING_TARGET_URL)
 
-logging_utils.log_progress("Data extraction complete. Initiating Transformation process")
+# Transform data
+logging_utils.log_progress("Data extraction complete. Transforming data...")
 transformed_df = transform_utils.transform(extracted_df, config.PATH_TO_EXCHANGE_RATE_CSV)
 
-logging_utils.log_progress("Data transformation complete. Initiating Loading process")
+# Create connection to output DB
+logging_utils.log_progress(f"Data transformation complete. Connecting to DB in '{config.PATH_TO_OUTPUT_DB}'...")
 sql_connection = sqlite3.connect(config.PATH_TO_OUTPUT_DB)
-logging_utils.log_progress(f"SQL Connection initiated, '{config.PATH_TO_OUTPUT_DB}'")
+
+# Load data
+logging_utils.log_progress("DB connection established. Loading data...")
 load_utils.load(transformed_df, config.PATH_TO_OUTPUT_CSV, config.OUTPUT_TABLE_NAME, sql_connection)
 
-run_query(f"SELECT * FROM {config.OUTPUT_TABLE_NAME}", sql_connection)
-run_query(f"SELECT AVG(MC_GBP_Billion) FROM {config.OUTPUT_TABLE_NAME}", sql_connection)
-run_query(f"SELECT Name FROM {config.OUTPUT_TABLE_NAME} LIMIT 5", sql_connection)
+# Check DB contents by querying
+db_utils.run_query(f"SELECT * FROM {config.OUTPUT_TABLE_NAME}", sql_connection)
+db_utils.run_query(f"SELECT AVG(MC_GBP_Billion) FROM {config.OUTPUT_TABLE_NAME}", sql_connection)
+db_utils.run_query(f"SELECT Name FROM {config.OUTPUT_TABLE_NAME} LIMIT 5", sql_connection)
 
+# Finalize processing
 sql_connection.close()
 logging_utils.log_progress("SQL Connection closed")
 logging_utils.log_progress("ETL process finished\n")
---
+++ utils/db_utils/db_utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from ..logging_utils import logging_utils
+
+def run_query(query_statement, sql_connection):
+    logging_utils.log_progress(f"run_query(): started, query='{query_statement}'")
+    query_output = pd.read_sql(query_statement, sql_connection)
+
+    logging_utils.log_progress(f"run_query(): query: {query_statement}")
+    logging_utils.log_progress(f"run_query(): output:\n{query_output.to_string()}")
+    logging_utils.log_progress(f"Process Complete")
